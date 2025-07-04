afeceef28b83e3b659b04d8bf30e6a024d4d1bad
adevcorps-0 <w.mkl.corp@gmail.com>
2025-07-04 10:53:11 +0200

    update for mongo

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ app.use('/webhook', bodyParser.raw({ type: 'application/json' }));
 app.use('/webhook', webhookRoutes);
 app.use('/xero', xeroRoutes);
 
+console.log("Mongo url ========= : ", process.env.MONGO_URI);
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
