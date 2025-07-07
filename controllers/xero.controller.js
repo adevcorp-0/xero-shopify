e1a1bd42329bb34decb7ef3bca150c52439a70e5
adevcorps-0 <w.mkl.corp@gmail.com>
2025-07-07 11:50:27 +0200

    again it

--- controllers/xero.controller.js
+++ controllers/xero.controller.js
@@ -6,7 +6,7 @@ const axios = require('axios');
 exports.redirectToXero = (req, res) => {
     console.log("Thsi is")
     const state = crypto.randomBytes(8).toString('hex');
-    const url = `https://login.xero.com/identity/connect/authorize?response_type=code&client_id=${XERO_CLIENT_ID}&redirect_uri=${encodeURIComponent(XERO_REDIRECT_URI)}&scope=openid profile email accounting.settings accounting.contacts accounting.transactions offline_access&state=${state}`;
+    const url = `https://login.xero.com/identity/connect/authorize?response_type=code&client_id=${XERO_CLIENT_ID}&redirect_uri=${encodeURIComponent(XERO_REDIRECT_URI)}&scope=openid profile email accounting.settings accounting.contacts accounting.transactions offline_access&state=${state}&prompt=consent`;
     res.redirect(url);
 };
 
